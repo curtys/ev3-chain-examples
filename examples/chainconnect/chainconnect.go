4ebefe417070b0e4ceb5071676f4513e28ac3c61
Simon Curty <simon.curty@unifr.ch>
2022-07-27 14:42:28 +0200

    simple connection test

---
+++ examples/chainconnect/chainconnect.go
@@ -0,0 +1,22 @@
+﻿package main
+
+import (
+	"context"
+	"ev3-eth/chain"
+	"flag"
+	"log"
+)
+
+func main() {
+	configPtr := flag.String("config", "config.toml", "Path to program configuration file")
+	flag.Parse()
+
+	config := chain.ReadConfig(*configPtr)
+	ctx := chain.BindContext(config)
+	num, err := ctx.Client().BlockNumber(context.Background())
+	if err != nil {
+		log.Fatalf("Could not get block number: %v\n", err)
+	} else {
+		log.Println(num)
+	}
+}
